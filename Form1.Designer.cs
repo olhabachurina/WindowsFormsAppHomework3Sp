ae797d405763ad702e422e2ed4dd05fb383ea606
Olga Bachurina <bachuri12@ukr.net>
2024-02-18 15:27:35 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsFormsAppHomework3Sp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMoney = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPercent = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMoney
+            // 
+            this.txtMoney.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtMoney.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtMoney.Location = new System.Drawing.Point(51, 54);
+            this.txtMoney.Name = "txtMoney";
+            this.txtMoney.Size = new System.Drawing.Size(205, 27);
+            this.txtMoney.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtName.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtName.Location = new System.Drawing.Point(51, 123);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(205, 27);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtPercent
+            // 
+            this.txtPercent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtPercent.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtPercent.Location = new System.Drawing.Point(51, 193);
+            this.txtPercent.Name = "txtPercent";
+            this.txtPercent.Size = new System.Drawing.Size(205, 27);
+            this.txtPercent.TabIndex = 2;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Blue;
+            this.btnUpdate.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnUpdate.Location = new System.Drawing.Point(72, 277);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(173, 39);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // lstLog
+            // 
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.ItemHeight = 16;
+            this.lstLog.Location = new System.Drawing.Point(333, 29);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(402, 372);
+            this.lstLog.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Blue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(47, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Money";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Blue;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(47, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 22);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Blue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(47, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 22);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Percent";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstLog);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtPercent);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtMoney);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMoney;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPercent;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.ListBox lstLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace WindowsFormsAppHomework3Sp
+{
+    public partial class Form1 : Form
+    {
+        private Bank _bank;
+        public Form1()
+        {
+            InitializeComponent();
+            _bank = new Bank(0, "", 0, lstLog);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtMoney.Text, out int money) && int.TryParse(txtPercent.Text, out int percent))
+            {
+                _bank.Money = money;
+                _bank.Name = txtName.Text;
+                _bank.Percent = percent;
+            }
+            else
+            {
+                MessageBox.Show("Please, enter valid values for money and percent.");
+            }
+        }
+    }
+
+    
+    public class Bank
+    {
+        private int _money;
+        private string _name;
+        private int _percent;
+        private ListBox _lstLog; 
+        private readonly object _lock = new object();
+
+        public Bank(int money, string name, int percent, ListBox lstLog)
+        {
+            _money = money;
+            _name = name;
+            _percent = percent;
+            _lstLog = lstLog;
+        }
+
+        private void UpdateListBox(string text)
+        {
+            if (_lstLog.InvokeRequired)
+            {
+                _lstLog.Invoke(new Action<string>(UpdateListBox), text);
+            }
+            else
+            {
+                _lstLog.Items.Add(text);
+            }
+        }
+
+        public int Money
+        {
+            get => _money;
+            set
+            {
+                lock (_lock)
+                {
+                    _money = value;
+                    Task.Run(() => UpdateListBox($"Money updated: {_money}"));
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                lock (_lock)
+                {
+                    _name = value;
+                    Task.Run(() => UpdateListBox($"Name updated: {_name}"));
+                }
+            }
+        }
+
+        public int Percent
+        {
+            get => _percent;
+            set
+            {
+                lock (_lock)
+                {
+                    _percent = value;
+                    Task.Run(() => UpdateListBox($"Percent updated: {_percent}"));
+                }
+            }
+        }
+    }
+}
